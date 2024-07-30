833e34d8e32ca370f980de361d2d7db75fb69656
Huina1aosp <huinadiscordforhuina@gmail.com>
2024-07-30 17:12:01 +0900

    auto2.py 업데이트

--- auto2.py
+++ auto2.py
@@ -1,13 +1,18 @@
 file = open('gsidown2.sh', "w")
 file.write("""#!/bin/bash
+
+echo "build bot by supers"
+
 set -e 
 
-BP=$PWD/crdroid/patches
-BY=$PWD/crdroid/
+BP=$PWD/patches
+BY=$PWD/
 
 git() {
        echo "---> git cloning..."
        git clone https://Huina1aosp/GSI_BUILD
+       cd GSI_BUILD
+       echo
 }
 
 
@@ -16,4 +21,46 @@ pkg() {
         sudo apt-get install ccache git-core gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
         echo
 }
+
+initRepo() {
+            echo "---> init repo..."
+               repo init -u https://github.com/crdroidandroid/android.git -b 14.0
+            echo
+}
+
+XML() {
+      echo "--> Preparing XML..."
+      cp $BP/gapps.xml .repo/local_manifests
+      cp $BP/manifest.xml .repo/local_manifests
+
+      echo
+}
+
+sync() {
+       echo "--> Syncing...."
+       repo sync
+       echo
+}
+
+applyPatches() {
+    echo "--> Applying patches"
+       bash patches/apply-patches.sh .
+    echo
+}
+
+START=$(date +%s)
+
+git
+pkg
+initRepo
+sync
+applyPatches
+
+END=$(date +%s)
+ELAPSEDM=$(($(($END-$START))/60))
+ELAPSEDS=$(($(($END-$START))-$ELAPSEDM*60))
+
+echo "--> Buildbot completed in $ELAPSEDM minutes and $ELAPSEDS seconds"
+echo
+
 
