fdd3a21ab735611bf52a1cd47be466dbebc15ee0
Huina1aosp <huinadiscordforhuina@gmail.com>
2024-07-30 16:49:29 +0900

    auto2.py 업데이트

--- auto2.py
+++ auto2.py
@@ -5,3 +5,15 @@ set -e
 BP=$PWD/crdroid/patches
 BY=$PWD/crdroid/
 
+git() {
+       echo "---> git cloning..."
+       git clone https://Huina1aosp/GSI_BUILD
+}
+
+
+pkg() {
+        echo "--> downloading pkg..."
+        sudo apt-get install ccache git-core gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
+        echo
+}
+
