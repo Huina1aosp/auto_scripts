573ec6d41fec398815f56ebc7098803d6ff32487
Huina1aosp <huinadiscordforhuina@gmail.com>
2024-08-02 01:37:23 +0900

    Update auto2.py

--- auto2.py
+++ auto2.py
@@ -14,7 +14,7 @@ cd GSI_BUILD
 
 pkg() {
         echo "--> downloading pkg..."
-        sudo apt-get install ccache repo git-core gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
+        sudo apt-get install ccache repo git-lfs git-core gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
         echo
 }
 
